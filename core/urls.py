712e7b0c17b2e2488dbba66f4643e9e3597c0f4c
xeryar <sheryarbaloch67@gmail.com>
2025-01-23 23:38:17 +0500

    Add debug URL patterns for Silk and media serving in urls.py

--- core/urls.py
+++ core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -24,3 +26,7 @@ urlpatterns = [
     # ? Auth
     path("api-auth/", include("rest_framework.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

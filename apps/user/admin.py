2f39fcf28072ba177170b3ae975d8e7b1e7c96ba
xeryar <sheryarbaloch67@gmail.com>
2025-01-24 22:56:16 +0500

    Register UserProfile, Role, and Permission models in the admin site

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from apps.user.models.role_permission_models import Permission, Role
+from apps.user.models.user_models import UserProfile
+
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Role)
+admin.site.register(Permission)

a5ae0181efd785b95ba9e7166acd458754ed08ca
xeryar <sheryarbaloch67@gmail.com>
2025-01-25 08:20:32 +0500

    Add viewsets for Project and Task with form validation for create and update operations

---
+++ apps/task/views/task_views.py
@@ -0,0 +1,53 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from apps.task.forms.task_forms import ProjectForm, TaskForm
+from apps.task.models.task_models import Project, Task
+from apps.task.serializers.task_serializers import ProjectSerializer, TaskSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def create(self, request, *args, **kwargs):
+        form = ProjectForm(request.data)
+        if form.is_valid():
+            project = form.save()
+            serializer = self.get_serializer(project)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        form = ProjectForm(request.data, instance=instance)
+        if form.is_valid():
+            project = form.save()
+            serializer = self.get_serializer(project)
+            return Response(serializer.data)
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def create(self, request, *args, **kwargs):
+        form = TaskForm(request.data)
+        if form.is_valid():
+            task = form.save()
+            serializer = self.get_serializer(task)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        form = TaskForm(request.data, instance=instance)
+        if form.is_valid():
+            task = form.save()
+            serializer = self.get_serializer(task)
+            return Response(serializer.data)
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)

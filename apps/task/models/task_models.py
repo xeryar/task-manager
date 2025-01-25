5d4aa3bcea65fd799ae29343bf4a808bdd8d48e6
xeryar <sheryarbaloch67@gmail.com>
2025-01-25 08:20:24 +0500

    Update Task model to change priority field to CharField with default value and blank option

--- apps/task/models/task_models.py
+++ apps/task/models/task_models.py
@@ -29,7 +29,7 @@ class Task(BaseModel):
     id (PK): AutoField
     title: CharField
     due_date: DateTimeField
-    priority: IntegerField
+    priority: CharField (choices: low, medium, high)
     status: CharField (choices: pending, in_progress, completed) (default: pending)
     """
 
@@ -40,8 +40,8 @@ class Task(BaseModel):
     PRIORITY_CHOICES = [("low", "Low"), ("medium", "Medium"), ("high", "High")]
     STATUS_CHOICES = [("pending", "Pending"), ("in_progress", "In Progress"), ("completed", "Completed")]
 
-    priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES)
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="pending")
+    priority = models.CharField(max_length=50, choices=PRIORITY_CHOICES, default="low", blank=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default="pending", blank=True)
 
     class Meta:
         app_label = "task"

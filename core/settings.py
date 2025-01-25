64d989e3e1e8f60bdd87f6e3dce985afc8fdb7b5
xeryar <sheryarbaloch67@gmail.com>
2025-01-25 05:00:17 +0500

    Add email configuration settings for SMTP backend

--- core/settings.py
+++ core/settings.py
@@ -211,3 +211,15 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# ---------------------------------------------------------------------------- #
+#                              EMAIL CONFIGURATION                             #
+# ---------------------------------------------------------------------------- #
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "Task Manager <no-reply@gmail.com>"

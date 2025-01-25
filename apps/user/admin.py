d1f93558c442a3405bd91eb0f23cfedba9185c74
xeryar <sheryarbaloch67@gmail.com>
2025-01-25 07:02:42 +0500

    Register RolePermission model in admin interface

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from apps.user.models.role_permission_models import Permission, Role
+from apps.user.models.role_permission_models import Permission, Role, RolePermission
 from apps.user.models.user_models import UserProfile
 
 admin.site.register(UserProfile)
 admin.site.register(Role)
 admin.site.register(Permission)
+admin.site.register(RolePermission)
